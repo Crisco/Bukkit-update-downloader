c619adc0d72d5c90e9c51f6a0e55afdbbca928a7
Oliver <crisco552@gmail.com>
2013-03-24 18:36:57 -0600

    started the current version code

--- update-bukkit.py
+++ update-bukkit.py
@@ -6,6 +6,12 @@ try:
 except ImportError:
 	from urllib.request import urlretrieve
 
+try:
+	from urllib import urlopen
+except ImportError:
+	from urllib.request import urlopen
+
+import hashlib
 from os import getcwd
 
 #Prints the current progress when downloading
@@ -14,16 +20,32 @@ def Progress(current, blockSize, total):
 	sys.stdout.write("\rPercent Done: %2d%%\r" % percent)
 	sys.stdout.flush()
 
+def isUpToDate(version):
+	pageSource
+	if version != "rel":
+		pageSource=urlopen("http://dl.bukkit.org/downloads/craftbukkit/list/"+version+"/").read()
+		version = "-" + version		
+	else:
+		pageSource=urlopen("http://dl.bukkit.org/downloads/craftbukkit/list/rb/").read()
+		version = ""
+	try:
+		downloaded = open("craftbukkit"+version+".jar", r)
+	except IOError:
+		return false
+	dMD5 = hashlib.md5(downloaded.read()).hexdigest()
+	
+
+
 def downloadFile(type): #downloads the dev and beta crafbukkit versions
 	print("Downloading \"craftbukkit-"+type+".jar\" to " + getcwd())
-	urlretrieve("http://dl.bukkit.org/latest-"+type+"/craftbukkit-"+type+".jar", "craftbukkit-"+type+".jar", reporthook=Progress)
+	urlretrieve("http://dl.bukkit.org/latest-"+type+"/craftbukkit-"+type+".jar", "craftbukkit-"+type+".jar", Progress)
 	print("\nDone!")
 
 if len(sys.argv)<2:
 	print("USAGE: update-bukkit <rel,beta,dev>")
 elif sys.argv[1]=="rel": #since the release bukkit format is very different from the other two I just hardcoded it
 	print("Downloading \"craftbukkit.jar\" to " + getcwd())
-	urlretrieve("http://dl.bukkit.org/latest-rb/craftbukkit.jar", "craftbukkit.jar", reporthook=Progress)
+	urlretrieve("http://dl.bukkit.org/latest-rb/craftbukkit.jar", "craftbukkit.jar", Progress)
 	print("\nDone!")
 elif sys.argv[1]=="dev" or sys.argv[1]=="beta":
 	downloadFile(str(sys.argv[1]))
